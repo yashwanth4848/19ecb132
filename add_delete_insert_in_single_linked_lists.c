71f5fcbe14d420548199e8bc8e5bbcd0b40f3e0a
yashwanth4848 <101689957+yashwanth4848@users.noreply.github.com>
2022-06-01 09:35:41 +0530

    Update add_delete_insert_in_single_linked_lists.c

--- add_delete_insert_in_single_linked_lists.c
+++ add_delete_insert_in_single_linked_lists.c
@@ -54,16 +54,98 @@ void insertAtEnd(struct Node** head_ref, int new_data) {
 }
 
 // Delete a node
-void deleteNode(struct Node** head_ref, int key) 
-{
+void deleteNode(struct Node** head_ref, int key) {
   struct Node *temp = *head_ref, *prev;
-  }
-if (temp != NULL && temp->data == key) 
-{
+
+  if (temp != NULL && temp->data == key) {
   *head_ref = temp->next;
   free(temp);
   return;
   }
-/*
+  // Find the key to be deleted
+  while (temp != NULL && temp->data != key) {
+  prev = temp;
+  temp = temp->next;
+  }
 
-*/
+  // If the key is not present
+  if (temp == NULL) return;
+
+  // Remove the node
+  prev->next = temp->next;
+
+  free(temp);
+}
+
+// Search a node
+int searchNode(struct Node** head_ref, int key) {
+  struct Node* current = *head_ref;
+
+  while (current != NULL) {
+  if (current->data == key) return 1;
+  current = current->next;
+  }
+  return 0;
+}
+
+// Sort the linked list
+void sortLinkedList(struct Node** head_ref) {
+  struct Node *current = *head_ref, *index = NULL;
+  int temp;
+
+  if (head_ref == NULL) {
+  return;
+  } else {
+  while (current != NULL) {
+    // index points to the node next to current
+    index = current->next;
+
+    while (index != NULL) {
+    if (current->data > index->data) {
+      temp = current->data;
+      current->data = index->data;
+      index->data = temp;
+    }
+    index = index->next;
+    }
+    current = current->next;
+  }
+  }
+}
+
+// Print the linked list
+void printList(struct Node* node) {
+  while (node != NULL) {
+  printf(" %d ", node->data);
+  node = node->next;
+  }
+}
+
+// Driver program
+int main() {
+  struct Node* head = NULL;
+
+  insertAtEnd(&head, 1);
+  insertAtBeginning(&head, 2);
+  insertAtBeginning(&head, 3);
+  insertAtEnd(&head, 4);
+  insertAfter(head->next, 5);
+
+  printf("Linked list: ");
+  printList(head);
+
+  printf("\nAfter deleting an element: ");
+  deleteNode(&head, 3);
+  printList(head);
+
+  int item_to_find = 3;
+  if (searchNode(&head, item_to_find)) {
+  printf("\n%d is found", item_to_find);
+  } else {
+  printf("\n%d is not found", item_to_find);
+  }
+
+  sortLinkedList(&head);
+  printf("\nSorted List: ");
+  printList(head);
+}
